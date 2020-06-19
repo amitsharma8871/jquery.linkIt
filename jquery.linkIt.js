1a76b765c30b6435fe4533d85d86b7a1ce6a0ff3
amit sharma <56514894+amitsharma8871@users.noreply.github.com>
2020-06-19 17:42:41 +0530

    Add files via upload

---
+++ jquery.linkIt.js
@@ -0,0 +1,38 @@
+/*
+* name    :  LinkIt
+* author  :  Amit Sharma
+* version :  0.1.0
+* license :  MIT
+*/
+
+(function($){
+    $.fn.linkIt = function(options){
+	
+        //Default setting
+        var settings = $.extend({
+            href        : null,
+            text        : null,
+            target      : `_self`
+
+        } , options);
+
+                    
+        //Validation
+        if(settings.href == null){
+            console.log('You need an href option for to work');
+            return this;
+        }
+
+        return this.each( function() { 
+
+            var object = $(this);
+            if (settings.text == null) {
+                settings.text = object.text();
+            }
+             
+            object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text);
+        });
+    }
+       
+    
+}(jQuery));
